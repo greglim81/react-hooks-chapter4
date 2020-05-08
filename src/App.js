e5909b39e128a319b0a2eb35f128c1e86627cdce
Greg Lim <limjunqu@gmail.com>
2020-05-08 12:14:55 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Rating from './Rating';
+import Products from './Products';
 
 function App() {
 
   return (
     <div>
-        <Rating rating='1' />
-        <Rating rating='2' />
-        <Rating rating='3' />
-        <Rating rating='4' />
-        <Rating rating='5' />
+        <Products />   
     </div>
   );
 }
